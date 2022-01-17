9f9e228b3f4d8d46502056af16dd3c7ff4bda74b
unknown <you@example.com>
2022-01-17 11:05:53 +0100

    downloading and using fonts with usefonts

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Home from "./screens/home";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
   return <Home />;
 }
