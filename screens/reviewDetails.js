4dd802d616641b5582f2c06e5c84d39f29f019a9
Alan Pecenkovic <alanquad@yahoo.com>
2022-01-17 12:57:21 +0100

    passing data between screens with navigation navigate and receiving it with navigation getParam method on other screen

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,15 +2,12 @@ import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails(props) {
-  const handlePress = () => {
-    props.navigation.goBack();
-  };
-
+export default function ReviewDetails({ navigation }) {
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails screen</Text>
-      <Button title="Go to the Home page" onPress={handlePress} />
+      <Text>{navigation.getParam("title")}</Text>
+      <Text>{navigation.getParam("body")}</Text>
+      <Text>{navigation.getParam("rating")}</Text>
     </View>
   );
 }
