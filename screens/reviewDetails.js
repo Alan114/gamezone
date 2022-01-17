b70528ee9ed119b28b91e51f1518a4136fed19bf
Alan Pecenkovic <alanquad@yahoo.com>
2022-01-17 12:31:37 +0100

    Making the navigation work using the navigation props navigate method

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails() {
+export default function ReviewDetails(props) {
+  const handlePress = () => {
+    props.navigation.goBack();
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text>ReviewDetails screen</Text>
+      <Button title="Go to the Home page" onPress={handlePress} />
     </View>
   );
 }
